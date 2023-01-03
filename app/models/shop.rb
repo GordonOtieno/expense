38233919313c1077b5fb5d41abc99afb5a8e0215
GordonOtieno <otienogordon95@gmail.com>
2023-01-03 21:58:38 +0300

    Modify tables to suite project needs

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,7 @@
-class Expense < ApplicationRecord
+class Shop < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :groups, through: :group_expenses
-  has_many :group_expenses, dependent: :destroy
+  has_many :groups, through: :group_shops
+  has_many :group_shops, dependent: :destroy
 
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, presence: true, length: { in: 2..100 }
