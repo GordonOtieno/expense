ae28e5347131a69fdf4996283570d2a58005ca07
GordonOtieno <otienogordon95@gmail.com>
2023-01-03 22:05:38 +0300

    Solved linters errors

--- db/schema.rb
+++ db/schema.rb
@@ -9,63 +9,64 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_162551) do
+# rubocop:disable Metrics/BlockLength
+ActiveRecord::Schema[7.0].define(version: 20_230_103_162_551) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "group_shops", force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "shop_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_group_shops_on_group_id"
-    t.index ["shop_id"], name: "index_group_shops_on_shop_id"
+  create_table 'group_shops', force: :cascade do |t|
+    t.bigint 'group_id', null: false
+    t.bigint 'shop_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['group_id'], name: 'index_group_shops_on_group_id'
+    t.index ['shop_id'], name: 'index_group_shops_on_shop_id'
   end
 
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.text "icon"
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_groups_on_author_id"
+  create_table 'groups', force: :cascade do |t|
+    t.string 'name'
+    t.text 'icon'
+    t.bigint 'author_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_groups_on_author_id'
   end
 
-  create_table "shops", force: :cascade do |t|
-    t.string "name"
-    t.integer "amount"
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_shops_on_author_id"
+  create_table 'shops', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'amount'
+    t.bigint 'author_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_shops_on_author_id'
   end
 
-  create_table "usage", force: :cascade do |t|
-    t.string "name"
-    t.integer "amount"
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_usage_on_author_id"
+  create_table 'usage', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'amount'
+    t.bigint 'author_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_usage_on_author_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "group_shops", "groups"
-  add_foreign_key "group_shops", "shops"
-  add_foreign_key "groups", "users", column: "author_id"
-  add_foreign_key "shops", "users", column: "author_id"
-  add_foreign_key "usage", "users", column: "author_id"
+  add_foreign_key 'group_shops', 'groups'
+  add_foreign_key 'group_shops', 'shops'
+  add_foreign_key 'groups', 'users', column: 'author_id'
+  add_foreign_key 'shops', 'users', column: 'author_id'
+  add_foreign_key 'usage', 'users', column: 'author_id'
 end
+# rubocop:enable Metrics/BlockLength
--- spec/requests/group_shops_spec.rb
+++ spec/requests/group_shops_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "GroupShops", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/group_shops/new"
+RSpec.describe 'GroupShops', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/group_shops/new'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/group_shops/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/group_shops/create'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
